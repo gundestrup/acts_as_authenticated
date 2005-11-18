7095801d84494c1f5611d2095287e7b935d41f87
technoweenie <technoweenie>
2005-11-18 20:26:16 +0000

    allow changing of the observer model name [Bill Katz]

--- generators/authenticated_mailer/templates/observer.rb
+++ generators/authenticated_mailer/templates/observer.rb
@@ -1,10 +1,10 @@
-class UserObserver < ActiveRecord::Observer
-  def before_save(user)
-    @signup = user.new_record?
+class <%= class_name %>Observer < ActiveRecord::Observer
+  def before_save(<%= file_name %>)
+    @signup = <%= file_name %>.new_record?
   end
 
-  def after_save(user)
-    UserNotifier.deliver_signup_notification(user) if @signup
-    UserNotifier.deliver_activation(user) if user.recently_activated?
+  def after_save(<%= file_name %>)
+    <%= class_name %>Notifier.deliver_signup_notification(<%= file_name %>) if @signup
+    <%= class_name %>Notifier.deliver_activation(<%= file_name %>) if <%= file_name %>.recently_activated?
   end
 end
